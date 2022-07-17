5f50c381f8a0068bf878a1cc33666c10d2bec15e
weqy <109391494+weqy@users.noreply.github.com>
2022-07-17 18:54:00 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+adj = input("Enter an Adjective: ")
+noun = input("Enter a Noun: ")
+pluralNoun = input("Enter a plural noun: ")
+female = input("Enter a Female Name: ")
+adj2 = input("Enter another Adjective: ")
+location = input("Enter a Location: ")
+
+print("There are many " + adj + " ways to choose a/an " + noun + " to read. First, you could ask for recommendations from your friends and " + pluralNoun + ".")
+print("Just don't ask Aunt " + female + "—she only reads " + adj2 + " magazines. If your friends and " + pluralNoun + " are no help, try checking out the " + location + ".")
