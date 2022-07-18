5e30767942e49b0dcfbf5e87b4672221b9813751
weqy <109391494+weqy@users.noreply.github.com>
2022-07-17 20:19:20 -0400

    Update and rename main.py to maingamecode.py

---
+++ maingamecode.py
@@ -0,0 +1,11 @@
+adj = input("Enter an Adjective: ") # 1st blank in mad libs
+noun = input("Enter a Noun: ") # 2nd blank in mad libs
+pluralNoun = input("Enter a plural noun: ") # 3rd blank in mad libs
+female = input("Enter a Female Name: ") # 4th blank in mad libs
+adj2 = input("Enter a Negative Adjective: ") # 5th blank in mad libs
+location = input("Enter a Location: ") # 6th blank in mad libs
+
+print("There are many " + adj + " ways to choose a/an " + noun + " to read. First, you could ask for recommendations from your friends and " + pluralNoun + ".") 
+# result of fill-in-the-blank
+print("Just don't ask Aunt " + female + "—she only reads " + adj2 + " magazines. If your friends and " + pluralNoun + " are no help, try checking out the " + location + ".")
+# result of fill-in-the-blank 2
